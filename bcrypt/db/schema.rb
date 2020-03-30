cd090efefd00ac41c4b2867cd8df4fcbe764db1c
abrandao <andersonbrandaolustosa@gmail.com>
2020-03-30 10:31:51 -0300

    User model

---
+++ bcrypt/db/schema.rb
@@ -0,0 +1,13 @@
+ActiveRecord::Schema.define(version: 2020_03_30_132035) do
+
+  enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
