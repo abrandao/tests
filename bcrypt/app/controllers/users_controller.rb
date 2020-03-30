cdb2325fd7fdc88bc8089e535f583042cb0a62f3
abrandao <andersonbrandaolustosa@gmail.com>
2020-03-30 11:06:44 -0300

    Session and User controllers

---
+++ bcrypt/app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+
+  def new
+  
+  end
+
+  def create
+    user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
+    )
+    if user.save
+      session[:user_id] = user.id
+      flash[:success] = "Succesfully created user!"
+      redirect_to '/contacts'
+    else
+      flash[:warning] = "Invalid email or password"
+      redirect_to '/signup'
+    end
+  end
+
+end
--- bcrypt/config/routes.rb
+++ bcrypt/config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
-  
+
+  get '/login' => 'session#new'
+  get '/login' => 'session#create'
+  get '/logout' => 'session#destroy'
+
 end
